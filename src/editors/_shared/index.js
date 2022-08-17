51cab26ec0a9bcf6617cc4bedcd19d0af3300af6
Rian Martins <rian.martins@mov.ai>
2022-08-17 13:28:40 +0000

    Export shared components

---
+++ src/editors/_shared/index.js
@@ -0,0 +1,17 @@
+import DetailsMenu from "./DetailsMenu/DetailsMenu";
+import CollapsibleHeader from "./CollapsibleHeader/CollapsibleHeader";
+import { HtmlTooltip } from "./HtmlTooltip/HtmlTooltip";
+import KeyValueTable from "./KeyValueTable/KeyValueTable";
+import MaterialTable from "./MaterialTable/MaterialTable";
+import MaterialTree from "./MaterialTree/MaterialTree";
+import Search from "./Search/Search";
+
+export {
+  DetailsMenu,
+  CollapsibleHeader,
+  HtmlTooltip,
+  KeyValueTable,
+  MaterialTable,
+  Search,
+  MaterialTree
+};
