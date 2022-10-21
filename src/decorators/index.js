199c3d817488a2f76e773a517091c1a324e56f2b
Manuel Nogueira <manuel.nogueira@mov.ai>
2022-10-21 09:23:56 +0000

    Exported withMenuHandler

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -1,5 +1,6 @@
 import withAlerts from "./withAlerts";
 import { withTheme } from "./withTheme";
 import withKeyBinds from "./withKeyBinds";
+import withMenuHandler from "./withMenuHandler";
 
-export { withAlerts, withTheme, withKeyBinds };
+export { withAlerts, withTheme, withKeyBinds, withMenuHandler };
