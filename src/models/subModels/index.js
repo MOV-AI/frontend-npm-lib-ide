9d2256d8278a32dec6ef408f46307a8943f71a7d
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2022-12-02 11:22:57 +0000

    fixup! Added container configurations

--- src/models/subModels/index.js
+++ src/models/subModels/index.js
@@ -1,6 +1,7 @@
 import Command from "./Command/Command";
 import EnvVar from "./EnvVar/EnvVar";
+import ContainerConf from "./EnvVar/EnvVar";
 import Parameter from "./Parameter/Parameter";
 import Port from "./Port/Port";
 
-export { Command, EnvVar, Parameter, Port };
+export { Command, EnvVar, ContainerConf, Parameter, Port };
