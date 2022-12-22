8d041d39a83b862c42bba88142789c619d281395
Manuel Nogueira <manuel.nogueira@mov.ai>
2022-12-22 00:39:45 +0000

    Added a way to add the App Menu

--- src/plugins/views/MainMenu/styles.js
+++ src/plugins/views/MainMenu/styles.js
@@ -4,6 +4,19 @@ export const mainMenuStyles = makeStyles(theme => ({
   mainMenuHolder: {
     height: "100%"
   },
+  appsHolder: {
+    "& span[role='button']": {
+      padding: "0",
+      "&:hover": {
+        outline: "6px solid rgba(54, 181, 230, 0.08)"
+      }
+    },
+    "& hr": {
+      borderColor: theme.palette.grey[200],
+      borderRadius: "2px",
+      margin: "15px 0"
+    }
+  },
   icon: {
     color: theme.palette.primary.main,
     cursor: "pointer",
