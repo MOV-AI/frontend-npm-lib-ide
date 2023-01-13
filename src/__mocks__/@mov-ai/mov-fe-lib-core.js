fa33c550b0337ec117963d8c8b443171e1ab78e2
PedroCristovao-Movai <pedro.cristovao@mov.ai>
2023-01-13 09:29:55 +0000

    fix jest test

--- src/__mocks__/@mov-ai/mov-fe-lib-core.js
+++ src/__mocks__/@mov-ai/mov-fe-lib-core.js
@@ -1,3 +1,4 @@
-export const Utils = { randomId: () => Math.floor(100 * Math.random()) };
+import { Utils as UtilsOriginal } from "@mov-ai/mov-fe-lib-core"
+export const Utils = { randomId: UtilsOriginal.randomId };
 export const CONSTANTS = { GLOBAL_WORKSPACE: "global" }
 export const MasterDB = { subscribe: () => { } }
--- src/editors/Configuration/store/ConfigurationStore.test.js
+++ src/editors/Configuration/store/ConfigurationStore.test.js
@@ -1,6 +1,6 @@
 import Store from "./ConfigurationStore";
-import Model from "../models/Configuration";
 import { DBSubscriber } from "../../../store";
+import { Model } from "../../../models";
 
 test("Smoke test", () => {
   const obj = new Store();
