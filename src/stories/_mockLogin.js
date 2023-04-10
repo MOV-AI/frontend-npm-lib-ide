6ef1f21d9e66e4fe42971c6d8856fcce520a6bf0
Manuel Nogueira <manuel.nogueira@mov.ai>
2023-04-10 08:51:09 +0000

    Removed _mocklogin creds

--- src/stories/_mockLogin.js
+++ src/stories/_mockLogin.js
@@ -8,10 +8,8 @@ export const authParams = {
       response: _ => {
         // MUST BE A REAL TOKEN FROM MOVAI BE
         return {
-          refresh_token:
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJSZWZyZXNoIiwiaXNzIjoiYmFja2VuZCIsImlhdCI6MTY4MTExNjM4MywiZXhwIjoxNjgxNzIxMTgzLCJqdGkiOiI1MDQwOGMwYy0yM2Q2LTQxNDYtYjdlNi05YjFmNWMwNmY5YWIiLCJyZWZyZXNoX2lkIjoiIiwiZG9tYWluX25hbWUiOiJpbnRlcm5hbCIsImFjY291bnRfbmFtZSI6ImFkbWluIiwiY29tbW9uX25hbWUiOiJBZG1pbiIsInVzZXJfdHlwZSI6IklOVEVSTkFMIiwicm9sZXMiOlsiQURNSU4iXSwiZW1haWwiOiIiLCJzdXBlcl91c2VyIjp0cnVlLCJyZWFkX29ubHkiOmZhbHNlLCJzZW5kX3JlcG9ydCI6ZmFsc2V9.C8ZOvKwj0dak3k_SqFo41kwLvv8DMIDQXp0BvDKp4YU",
-          access_token:
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBY2Nlc3MiLCJpc3MiOiJiYWNrZW5kIiwiaWF0IjoxNjgxMTE2MzgzLCJleHAiOjE2ODExMTk5ODMsImp0aSI6IjBiY2EyM2YwLWU1NDctNDYwNS04ZDFjLWE2MmE3MzYwYzBkZSIsInJlZnJlc2hfaWQiOiI1MDQwOGMwYy0yM2Q2LTQxNDYtYjdlNi05YjFmNWMwNmY5YWIiLCJkb21haW5fbmFtZSI6ImludGVybmFsIiwiYWNjb3VudF9uYW1lIjoiYWRtaW4iLCJjb21tb25fbmFtZSI6IkFkbWluIiwidXNlcl90eXBlIjoiSU5URVJOQUwiLCJyb2xlcyI6WyJBRE1JTiJdLCJlbWFpbCI6IiIsInN1cGVyX3VzZXIiOnRydWUsInJlYWRfb25seSI6ZmFsc2UsInNlbmRfcmVwb3J0IjpmYWxzZX0.upHOOH-c31mB7akaBxv_RJvlyi_ZvuUGqJfexxumKFo",
+          refresh_token: "",
+          access_token: "",
           error: false
         };
       }
