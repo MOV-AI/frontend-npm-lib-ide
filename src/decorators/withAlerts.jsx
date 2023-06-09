8cd7280497aad73f700908fd360eba2b4079826d
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2023-06-09 14:26:14 +0100

    withAlerts call

--- src/decorators/withAlerts.jsx
+++ src/decorators/withAlerts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getRefComponent } from "../utils/Utils";
+import { call } from "../utils/noremix";
 import { PLUGINS } from "../utils/Constants";
 import { dialog } from "../plugins/Dialog/Dialog";
 
@@ -12,16 +13,11 @@ const withAlerts = Component => {
   const RefComponent = getRefComponent(Component);
 
   return (props, ref) => {
-    // Props
-    const { call } = props;
-
     /**
      * Create snackbar alert
      * @param {{title: String, message: String, location: String, severity: String}} alertData
      */
-    const alert = options => {
-      call(PLUGINS.ALERT.NAME, PLUGINS.ALERT.CALL.SHOW, options);
-    };
+    const alert = options => call(PLUGINS.ALERT.NAME, PLUGINS.ALERT.CALL.SHOW, options);
 
     return (
       <RefComponent
