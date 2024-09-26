59b96c73887fe7af7ca96653346f19d8cde37e18
Paulo Andre Azevedo Quirino <paulo.quirino@mov.ai>
2024-09-26 11:51:01 +0100

    wip

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -1,5 +1,4 @@
 import withAlerts from "./withAlerts";
-import withKeyBinds from "./withKeyBinds";
 import withMenuHandler from "./withMenuHandler";
 
-export { withAlerts, withKeyBinds, withMenuHandler };
+export { withAlerts, withMenuHandler };
